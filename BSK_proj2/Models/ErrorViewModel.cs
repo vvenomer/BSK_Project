53def0e8cb38fd485731e3c77fbf926a483a7537
vvenomer <vvenomer@gmail.com>
2019-03-08 23:27:21 +0100

    Add project files.

---
+++ BSK_proj2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BSK_proj2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
